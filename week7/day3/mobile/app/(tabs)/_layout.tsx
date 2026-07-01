df8cb53e3eb6f2fff76616816602cdd9c4e21d2a
shakerbr <shbhky@gmail.com>
2026-07-01 11:27:11 +0300

    feat: implement tab navigation with Home and About screens

--- week7/day3/mobile/app/(tabs)/_layout.tsx
+++ week7/day3/mobile/app/(tabs)/_layout.tsx
@@ -1,9 +1,21 @@
+import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{ headerShown: false }} />
+      <Tabs.Screen name="index" options={{ 
+        title: "Home",
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="home" color={color} size={size} />
+        ),
+      }} />
+      <Tabs.Screen name="about" options={{ 
+        title: "About",
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="information-circle" color={color} size={size} />
+        ),
+      }} />
     </Tabs>
   );
 }
